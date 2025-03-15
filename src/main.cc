391563da42737ffb132d97ceea69276a81985f3d
Suvan Sarkar <suvansarkar1@gmail.com>
2025-03-15 17:37:01 +0530

    Improve code modularity. Create a vec3 class for ease.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main() {
+#include "color.h"
+#include "vec3.h"
+
+int main(int argc, char** argv) {
 
   // Image descriptor
   int image_width = 256;
@@ -22,16 +25,8 @@ int main() {
   for(int j = 0; j < image_height; j++){
     std::clog << "\rScanlines remaining: " << image_height - j << std::flush;
     for (int i = 0; i < image_width; i++) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_width - 1);
-      auto b = 0.0f;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << ' ' << '\n';
-
+      auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+      write_color(std::cout, pixel_color);
     }
   }
 
