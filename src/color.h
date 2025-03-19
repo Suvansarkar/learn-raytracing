3924f39633fd3e8545de97066b4d77f313512eb4
Suvan Sarkar <suvansarkar1@gmail.com>
2025-03-20 00:23:59 +0530

    implement anti aliasing

--- src/color.h
+++ src/color.h
@@ -1,15 +1,17 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
 
-inline void write_color(std::ostream& out, const color& pixel_color) {
+void write_color(std::ostream& out, const color& pixel_color) {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
   auto b = pixel_color.z();
 
+  static const interval intensity(0.000, 0.999);
   int rbyte = int(255.999 * r);
   int gbyte = int(255.999 * g);
   int bbyte = int(255.999 * b);
